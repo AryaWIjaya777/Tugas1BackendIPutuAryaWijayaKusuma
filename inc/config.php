ec25922bc3720cf00d44c0dc9e135b9dad4c67c1
AryaWijaya777 <240030139@stikom-bali.ac.id>
2025-12-02 22:31:37 +0800

    menambahkan kode pada config.php untuk mengkonfigurasi dengan database mengkonfigurasi base url sesuai dengan localhost dan membuat menu navigasi lalu menambahkan autoload class dari folder class

--- inc/config.php
+++ inc/config.php
@@ -6,3 +6,21 @@ ini_set('display_errors', 1);
 if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'produk_crud');
+
+define('BASE_URL', 'http://localhost:8000/');
+
+const NAV_PAGES = [
+  ['title' => 'Home',          'url' => 'index.php'],
+  ['title' => 'Daftar Produk', 'url' => 'index.php'],
+  ['title' => 'Tambah Produk', 'url' => 'create.php']
+];
+
+spl_autoload_register(function ($class) {
+  $path = __DIR__ . '/../class/' . $class . '.php';
+  if (file_exists($path)) require_once $path;
+});
