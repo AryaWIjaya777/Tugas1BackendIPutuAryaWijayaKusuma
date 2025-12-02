da3636866cc939a3f53e77f8730b1308f1f19056
AryaWijaya777 <240030139@stikom-bali.ac.id>
2025-12-03 00:25:16 +0800

    Menambahkan isi kode pada file class Utility.php termasuk uniqueFilename() untuk membuat nama file unik saat upload dan e() untuk melakukan escape string agar aman ditampilkan di HTML.

---
+++ class/Utility.php
@@ -0,0 +1,15 @@
+<?php
+
+class Utility
+{
+  public static function uniqueFilename($original)
+  {
+    $ext = pathinfo($original, PATHINFO_EXTENSION);
+    return time() . '_' . bin2hex(random_bytes(6)) . '.' . $ext;
+  }
+
+  public static function e($str)
+  {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  }
+}
