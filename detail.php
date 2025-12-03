b63e7f54cb9c9e6e9c108880515ac693bb1c576e
AryaWijaya777 <240030139@stikom-bali.ac.id>
2025-12-03 15:50:11 +0800

    menambahkan html dasar pada detail.php yang menampilkan detail-detail yang dimiliki produk seperti id_produk, nama, harga, stok, kategori, status, dan gambarnya

--- detail.php
+++ detail.php
@@ -7,3 +7,31 @@ $produk = $produkModel->getById($_GET['id']);
 if (!$produk) die('Produk tidak ditemukan');
 
 $message = '';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Detail Produk</title>
+</head>
+
+<body>
+  <h1>Detail Produk</h1>
+  <p><a href="index.php">Kembali</a></p>
+  <ul>
+    <li>Id_produk: <?php echo $produk['id_produk']; ?></li>
+    <li>Nama: <?php echo Utility::e($produk['nama']); ?></li>
+    <li>Harga: <?php echo $produk['harga']; ?></li>
+    <li>Stok: <?php echo $produk['stok']; ?></li>
+    <li>Kategori: <?php echo Utility::e($produk['kategori']); ?></li>
+    <li>Status: <?php echo Utility::e($produk['status']); ?></li>
+    <li>Gambar: <?php if ($produk['gambar_path']): ?>
+        <img src="uploads/<?php echo Utility::e($produk['gambar_path']); ?>" width="100">
+      <?php endif; ?>
+    </li>
+  </ul>
+</body>
+
+</html>
